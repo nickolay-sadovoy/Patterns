32132ced782d87fb2af46bff6e7c6227f2cb8b7f
Nick Sadovoy <nickolay.sadovoy@gmail.com>
2020-07-25 11:01:37 +0300

    update

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PatternsApp.Creational.Singleton;
+
+
+namespace PatternsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Instance.Info("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Logger.Instance.Info($"Hello, {name}, on {date:d} at {date:t}!");
+            Console.ReadKey(true);
+        }
+    }
+}
